4c6a540c67573e5ae3611e0cd70fb08be9b62f20
blaues0cke <thomas@thomaskekeisen.de>
2013-08-04 14:03:08 +0200

    Initial commit.

---
+++ TkTFTFixer/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TkTFTFixer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+        }
+    }
+}
